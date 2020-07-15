144d0b6686a8774023b000d7dbf17e249ee663ae
AATHIL.TA <cluelessidiot@users.noreply.github.com>
2020-07-15 13:30:07 +0530

    added howMany

--- week2/stringSecondAssignments/part1/part1.java
+++ week2/stringSecondAssignments/part1/part1.java
@@ -6,7 +6,67 @@
  * @version (a version number or a date)
  */
 public class part1 {
+    int findStopCodon(String dna,int startIndex,String endCodon){
+        int endIndex=dna.indexOf(endCodon,startIndex+3);
+        if(endIndex==-1 || (endIndex-startIndex)%3==0){
+            return dna.length();
+        }
+        else{
+            //System.out.println(dna.substring(startIndex,endIndex+3));
+         return endIndex;
+        }
     
+    
+    
+    
+    }
+    String findGene(String dna){
+        dna=dna.toUpperCase();
+        int startIndex=dna.indexOf("ATG");
+        if(startIndex==-1){
+            return "";
+        }
+        int endIndexTaa=findStopCodon(dna,0,"TAA");
+        int endIndexTag=findStopCodon(dna,0,"TAG");
+        int endIndexTga=findStopCodon(dna,0,"TGA");
+        int minIndex=Math.min(endIndexTaa,Math.min(endIndexTag,endIndexTga));
+        if(minIndex==dna.length())
+           return "";
+        else
+            return dna.substring(startIndex,minIndex+3);
+            
+    
+    }
+    int howMany(String a,String b){
+        int startIndex=0;
+        int ct=0;
+        while(true){
+        
+        int index=a.indexOf(b,startIndex);
+        //System.out.println("index"+index);
+         if(index==-1){
+            return ct;
+            }
+            else{
+                ct=ct+1;
+                //System.out.println("ct"+);
+                startIndex=index+b.length();
+                if(startIndex+b.length()>a.length())
+                  return ct;
+            }
+       }
+    }
+    void testHowMany(){
+    
+    System.out.println(howMany("aaaaaaaaaaa","a"));
+    System.out.println(howMany("abaabaabaabaaba","aba"));
+    System.out.println(howMany("aaaaaaaaaaa","q"));
+    System.out.println(howMany("aaaaabaaaaaba","aba"));
+    }
+    void testFindStopCodon(){
+        
+        System.out.println(findGene("ATACATATATGTGTGTGGATGATTAATAG"));
+    }
     String findSimpleGene(String dna){
         String subString=null;
         dna=dna.toUpperCase();
